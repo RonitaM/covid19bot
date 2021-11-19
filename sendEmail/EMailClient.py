d9b07295b7d8c9a9e6df76ba8cf01c51b72afbe2
Ronita Mitra <86037961+RonitaM@users.noreply.github.com>
2021-11-19 10:01:04 +0530

    Update EMailClient.py

--- sendEmail/EMailClient.py
+++ sendEmail/EMailClient.py
@@ -11,8 +11,8 @@ class GMailClient:
     def sendEmail(self,contacts):
         #EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
         #EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
-        EMAIL_ADDRESS = 'sender email'
-        EMAIL_PASSWORD = 'sender password'
+        EMAIL_ADDRESS = 'riskassess123@gmail.com'
+        EMAIL_PASSWORD = 'Ronita@99'
 
         #contacts = ['dineshraturi22@gmail.com']
 
