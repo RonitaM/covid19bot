ece302c3efd105f7dc1f025d7269068f31179270
Ronita Mitra <86037961+RonitaM@users.noreply.github.com>
2021-11-19 17:39:15 +0530

    Update Conversations.py

--- saveConversation/Conversations.py
+++ saveConversation/Conversations.py
@@ -9,11 +9,11 @@ class Log:
         self.date = self.now.date()
         self.current_time = self.now.strftime("%H:%M:%S")
 
-        db = dbConn['Covid-19db']  # connecting to the database called crawlerD
+        #db = dbConn['Covid-19db']  # connecting to the database called crawlerD
         mydict = {"sessionID":sessionID,"User Intent" : intent ,"User": usermessage, "Bot": botmessage, "Date": str(self.date) + "/" + str(self.current_time)}
 
         #table = db[sessionID]
-        records = dbConn.cases_records
+        records = dbConn.chat_records
         records.insert_one(mydict)
 
         #table.insert_one(mydict)
@@ -25,12 +25,12 @@ class Log:
         cases_dict = {"search":search,"cases": botmessage}
         newvalues = {"$set": cases_dict}
 
-        records = dbConn.cases_records
+        records = dbConn.chat_records
         records.update_one(myquery, newvalues)
         #records.insert_one(cases_dict)
 
     def getcasesForEmail(self, search,botmessage,dbConn):
-        records = dbConn.cases_records
+        records = dbConn.chat_records
         print(records)
         return records.find_one({'search': search})
 
